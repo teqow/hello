014c2272bc4c589e36a8acbd43b1fdb102823e45
Bartosz Skowronek <bartosz.skowronek@softprime.pl>
2019-03-20 19:19:06 +0100

    Updates

---
+++ precache-manifest.e12fa7d0d74c1da1b13cf9743f147ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ded4da0fe71cbdcab24f",
+    "url": "/hello/static/js/runtime~main.f3b3c316.js"
+  },
+  {
+    "revision": "e8548261e1ac7ff6beee",
+    "url": "/hello/static/js/main.8237fe87.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/hello/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "e8548261e1ac7ff6beee",
+    "url": "/hello/static/css/main.07a0cab1.chunk.css"
+  },
+  {
+    "revision": "e2776aac217af820511a45a7dd8439cc",
+    "url": "/hello/index.html"
+  }
+];
